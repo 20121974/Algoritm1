66f84e1d3f65cf06b73c22314ec0cca6e42ca510
20121974 <nfi46102@mail.ru>
2022-12-09 11:42:53 +0300

    Commit 1

---
+++ src/test/java/com/skypro/group11/algoritm1/Algoritm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.group11.algoritm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algoritm1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
