b84ffddf1116b180ddab997776887289e90ebb88
20121974 <nfi46102@mail.ru>
2022-12-16 15:25:09 +0300

    Commit 4

--- src/main/java/com/skypro/group11/algoritm1/IntegerListImpl.java
+++ src/main/java/com/skypro/group11/algoritm1/IntegerListImpl.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.util.Arrays;
 
 public class IntegerListImpl implements IntegerList {
-    private final Spring[] storage;
+    private Spring[] storage;
     private int size;
 
     public IntegerListImpl() {
@@ -17,7 +17,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Spring add(Spring item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         storage[size++] = item;
         return item;
@@ -25,7 +25,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Spring add(int index, Spring item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         validateIndex(index);
         if (index == size) {
@@ -135,9 +135,9 @@ public class IntegerListImpl implements IntegerList {
         }
     }
 
-    private void validateSize() {
+    private void growIfNeeded() {
         if (size == storage.length) {
-            throw new StorageIsFullException();
+            grow();
         }
     }
 
@@ -148,15 +148,7 @@ public class IntegerListImpl implements IntegerList {
     }
 
     public void sort(Integer[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - 1];
-                j--;
-            }
-            arr[j] = temp;
-        }
+        quickSort(arr, 0, arr.length-1);
     }
     public boolean binarySearch(Integer[] arr, int item) {
         int min = 0;
@@ -176,5 +168,40 @@ public class IntegerListImpl implements IntegerList {
             }
         }
         return false;
+    }
+
+    private void grow() {
+        storage = Arrays.copyOf(storage, size + size / 2);
+    }
+
+    private void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private void swapElements(Integer[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 }
